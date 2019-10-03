ff314bf9bc042cb7ec2fb987471dc3dcfd105ea2
sparshg05 <55894372+sparshg05@users.noreply.github.com>
2019-10-03 19:16:03 +0530

    Add files via upload

---
+++ question5.py
@@ -0,0 +1,13 @@
+a=int(input('enter number'))
+while (a<0):
+    if (a<0):
+        print('negative change to zero')
+        a=0
+        a=int(input("enter no. again"))
+    else:
+        print(a)
+        
+print(a)
+
+
+x=input()
